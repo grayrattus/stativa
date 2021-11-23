4ec040f5d9534723f65760583fe99946ace9833f
arturdziedziczak0@gmail.com <arturdziedziczak0@gmail.com>
2021-11-23 21:03:28 +0100

    Add template that works on whole data set

---
+++ templates/smallImagesPartialTemplateAll.sh
@@ -0,0 +1,52 @@
+#/bin/sh
+NOT_USED_IMAGES="$SUB_IMAGES_IDS"
+
+cat << EOF >> $SUB_OUTPUT_INDEX
+	<style>
+	  [data-title]::before {
+	    content: attr(data-title) " ";
+	    font-size: 2em;
+	    text-align: center;
+	    display: block;
+	    margin: 1em;
+	  }
+	  [data-description]::after {
+	    content: attr(data-description) " ";
+	    margin: 0.5em;
+	    display: block;
+	  }
+	  .small-images-partial-template {
+	    display: flex;
+	    justify-content: center;
+	    flex-direction: column;
+	    align-items: center;
+	  }
+	</style>
+EOF
+
+i=0;
+rows=0;
+to=1;
+max=$(shuf -i 0-$to -n 1)
+for IMAGE_ID in $SUB_IMAGES_IDS; do
+  NOT_USED_IMAGES=$(echo $NOT_USED_IMAGES | sed -E "s/$IMAGE_ID\s?//g");
+  [ $i -eq 0 ] && echo '<div class="grid">' >> $SUB_OUTPUT_INDEX
+  cat << EOF >> $SUB_OUTPUT_INDEX
+      <div class="small-images-partial-template"><img id="$IMAGE_ID" style="max-height: 100vh;"/></div>
+EOF
+  if [ "$i" -eq "$max" ]; then 
+    echo '</div>' >> $SUB_OUTPUT_INDEX 
+    i=0
+    max=$(shuf -i 0-$to -n 1)
+    rows=$(( $rows + 1)) 
+    if [ -z "$NOT_USED_IMAGES" ]; then 
+      break
+    fi
+  else 
+    i=$(( $i + 1)) 
+  fi
+done
+[ $i -le 0 ] && echo '</div>' >> $SUB_OUTPUT_INDEX
+
+echo $NOT_USED_IMAGES
+
